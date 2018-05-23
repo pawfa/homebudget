2d29c300a7fb074061982487f708a6b78e90aa0f
Pawfa <fajfer.pawel@gmail.com>
2018-05-23 10:50:40 +0200

    First commit

---
+++ dataservice/src/main/java/com/homebudget/dataservice/DataserviceApplication.java
@@ -0,0 +1,12 @@
+package com.homebudget.dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataserviceApplication.class, args);
+	}
+}
